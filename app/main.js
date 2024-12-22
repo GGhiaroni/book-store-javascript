afc3d7f40a97324e4640a1d8f0ba439f4faf63a1
GGhiaroni <gghiaronitiziano@gmail.com>
2024-12-22 15:36:04 -0300

    inclusao da funcionalidade de aplicar descontos no preco dos livros

--- app/main.js
+++ app/main.js
@@ -7,24 +7,6 @@ getBuscarLivrosDaApi();
 async function getBuscarLivrosDaApi() {
     const res = await fetch(endpointApi);
     livros = await res.json();
-    console.log(livros);
-    inserirLivrosNoHtml(livros);
-}
-
-const sessionInserirLivros = document.getElementById("livros");
-
-function inserirLivrosNoHtml(listaDeLivros) {
-    listaDeLivros.forEach(livro => {
-        sessionInserirLivros.innerHTML += `
-        <div class="livro">
-      <img class="livro__imagens" src=${livro.imagem} alt=${livro.alt} />
-      <h2 class="livro__titulo">${livro.titulo}</h2>
-      <p class="livro__descricao">${livro.autor}</p>
-      <p class="livro__preco" id="preco">R$${livro.preco}</p>
-      <div class="tags">
-        <span class="tag">${livro.categoria}</span>
-      </div>
-    </div>
-    `;
-    });
+    const livrosComDesconto = aplicarDesconto(livros);
+    inserirLivrosNoHtml(livrosComDesconto);
 }
